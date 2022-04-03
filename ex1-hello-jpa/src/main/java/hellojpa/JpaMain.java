6838ec69c26498928e12a9c2b0ae0eac7daacdfd
Juice <3221ju@gmail.com>
2022-04-03 18:44:09 +0900

    [STU-1] 1.HelloJPA

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,62 @@
+package hellojpa;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // 트랜잭션 단위 마다 em 을 만들어서 요청하기
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // INSERT
+//            System.out.println("INSERT ---");
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("test1");
+//            em.persist(member);
+//            System.out.println("INSERT MEMBER id = " + member.getId() + ", name = " + member.getName());
+//            System.out.println("");
+
+            // SELECT
+            System.out.println("SELECT ---");
+            Member member1 = em.find(Member.class, 1L);
+            System.out.println("member id = 1 name = " + member1.getName());
+            System.out.println();
+
+            // SELECT BY QUERY
+            System.out.println("SELECT BY QUERY ---");
+            List<Member> resultList = em.createQuery("SELECT m FROM Member as m", Member.class).getResultList();
+            for (Member resultMember : resultList) {
+                System.out.println("SELECT BY QUERY  MEMBER id = " + resultMember.getId() + ", name = " + resultMember.getName());
+            }
+            System.out.println();
+
+            // UPDATE
+            System.out.println("UPDATE ---");
+            Member beforeMember = em.find(Member.class, 1L);
+            System.out.println("before hellojpa.Member Name: " + beforeMember.getName());
+
+            String updateName = "hello spring";
+            beforeMember.setName(updateName);
+            Member updatedMember = em.find(Member.class, 1L);
+            System.out.println("updated hellojpa.Member Name: " + updatedMember.getName());
+
+            // DELETE
+            //em.remove(em.find(Member.class, 1L));
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
